328ac85461d7b3b1318d86fd96564bb8b6f5f953
Emanuel Berger <emanuelberger49@gmail.com>
2025-02-15 00:20:07 +0100

    del everything

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/ui/Card.jsx
-export const Card = ({ children, className, onClick }) => (
-  <div onClick={onClick} className={`border p-4 rounded shadow ${className}`}>
-    {children}
-  </div>
-);
-
-export const CardContent = ({ children }) => <div>{children}</div>;
-
